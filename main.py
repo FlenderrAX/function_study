74c1d0ae2759af57b0e665e557901cb298421752
Amine Elb. <74816698+FlenderrAX@users.noreply.github.com>
2024-08-27 23:41:37 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sympy as sp
 
-x = sp.symbols('x')
+limit_symbol = sp.symbols('x')
 
 x_neighborhood = input("Enter the value of x for neighborhood (or 'oo' for infinity): ")
 x_neighborhood = sp.oo if x_neighborhood == 'oo' else -sp.oo if x_neighborhood == '-oo' else int(x_neighborhood)
-r = sp.limit(2*x - 7, x, x_neighborhood)
+r = sp.limit(2*limit_symbol - 7, limit_symbol, x_neighborhood)
 
 print(f"{r} is {'Infinite' if r == sp.oo or r == -sp.oo else 'not Infinite'}")
